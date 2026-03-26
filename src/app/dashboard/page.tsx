312724203d0e30825de8d6bfc6915886630f0050
jaydeb sarader <153087067+jaydeb2023@users.noreply.github.com>
2026-03-27 05:06:48 +0530

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,8 +5,7 @@ import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 import { TrendingUp, Users, Flame, Thermometer, Snowflake, Phone, Clock, CreditCard, MessageSquare, IndianRupee } from 'lucide-react'
-import type { BarChartProps, AreaChartProps } from 'recharts'
-import { AreaChart, Area, BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
+
 
 
 const weeklyData = [
