9245bd50181f6421454737a45516c0aaae9ab78f
jaydeb sarader <153087067+jaydeb2023@users.noreply.github.com>
2026-03-26 21:25:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Fixed: Moved out of experimental
   serverExternalPackages: ['twilio', 'razorpay', 'nodemailer', '@supabase/ssr'],
 
   images: {
     domains: ['api.dicebear.com', 'avatars.githubusercontent.com'],
-    // You can later upgrade to remotePatterns if you want
   },
 
   env: {
