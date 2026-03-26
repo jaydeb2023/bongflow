96a40401f7aaa196664b61c077f2b5ecbdf6bca9
jaydeb sarader <153087067+jaydeb2023@users.noreply.github.com>
2026-03-27 05:07:31 +0530

    Create charts.tsx

---
+++ src/lib/charts.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+import { AreaChart, Area, BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
+
+export { AreaChart, Area, BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer }
